NetLogo/microtortoise

commit e83f89527d01d75093f5c1ac4a1d3f850ac98247

Optimization/Major: Don't build updates when visual value is unchanged

typescript/src/model.ts
@@ -176,14 +176,18 @@ class Turtle extends Agent {
   }
 
   public rotate(degrees: number): void {
-    this.heading = (this.heading + (degrees + 360)) % 360;
-    this.recomputeDXY();
-    (Updater.turtles[this.who] ??= {} as TurtleUpdate).heading = this.heading;
+    const newHeading = (this.heading + (degrees + 360)) % 360;
+    if (this.heading !== newHeading) {
+      this.recomputeDXY();
+      (Updater.turtles[this.who] ??= {} as TurtleUpdate).heading = this.heading;
+    }
   }
 
   public setColor(value: number): void {
-    this.color = value;
-    (Updater.turtles[this.who] ??= {} as TurtleUpdate).color = value;
+    if (this.color !== value) {
+      this.color = value;
+      (Updater.turtles[this.who] ??= {} as TurtleUpdate).color = value;
+    }
   }
 
   public setSize(value: number): void {
@@ -266,8 +270,10 @@ class Patch extends Agent {
   }
 
   public setColor(value: number): void {
-    this.pcolor = value;
-    (Updater.patches[this.id] ??= {} as PatchUpdate).pcolor = value;
+    if (this.pcolor !== value) {
+      this.pcolor = value;
+      (Updater.patches[this.id] ??= {} as PatchUpdate).pcolor = value;
+    }
   }
 
   public setVar(name: string, value: NetLogoValue): void {

scala/src/main/Model.scala
@@ -249,25 +249,33 @@ class Turtle(val who: Int, shapeName: String) extends Agent {
 
   def rotate(degrees: Double): Unit = {
 
-    heading = (heading + degrees + 360.0) % 360.0
-    recomputeDXY()
+    val newHeading = (heading + degrees + 360.0) % 360.0
 
-    val mapping = TurtleKey.Heading -> heading
-    Updater.turtles.get(who).fold {
-      Updater.turtles += who -> MMap(mapping)
-    } {
-      _ += mapping
+    if (heading != newHeading) {
+
+      heading = newHeading
+      recomputeDXY()
+
+      val mapping = TurtleKey.Heading -> heading
+      Updater.turtles.get(who).fold {
+        Updater.turtles += who -> MMap(mapping)
+      } {
+        _ += mapping
+      }
+
     }
 
   }
 
   def setColor(value: Double): Unit = {
-    color = value
-    val mapping = TurtleKey.Color -> color
-    Updater.turtles.get(who).fold {
-      Updater.turtles += who -> MMap(mapping)
-    } {
-      _ += mapping
+    if (color != value) {
+      color = value
+      val mapping = TurtleKey.Color -> color
+      Updater.turtles.get(who).fold {
+        Updater.turtles += who -> MMap(mapping)
+      } {
+        _ += mapping
+      }
     }
   }
 
@@ -344,12 +352,14 @@ class Patch(idNum: Int, vars: Array[String], val pxcor: Int, val pycor: Int) ext
     variables(name)
 
   def setColor(value: Double): Unit = {
-    pcolor = value
-    val mapping = PatchKey.PColor -> pcolor
-    Updater.patches.get(idNum).fold {
-      Updater.patches += idNum -> MMap(mapping)
-    } {
-      _ += mapping
+    if (pcolor != value) {
+      pcolor = value
+      val mapping = PatchKey.PColor -> pcolor
+      Updater.patches.get(idNum).fold {
+        Updater.patches += idNum -> MMap(mapping)
+      } {
+        _ += mapping
+      }
     }
   }
 

commit 97c28a9caf3ac22e8c8f1e616f33b3532fc0d136

Bugfix/Major: Allow heading of turtles to change again
Regression introduced in e83f89527d01d75093f5c1ac4a1d3f850ac98247

typescript/src/model.ts
@@ -178,6 +178,7 @@ class Turtle extends Agent {
   public rotate(degrees: number): void {
     const newHeading = (this.heading + (degrees + 360)) % 360;
     if (this.heading !== newHeading) {
+      this.heading = newHeading;
       this.recomputeDXY();
       (Updater.turtles[this.who] ??= {} as TurtleUpdate).heading = this.heading;
     }